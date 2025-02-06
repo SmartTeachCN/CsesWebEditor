a97a9255ec45d9c3e9b9893fa2d46af1b05da0b5
pylxu <e-pylxu@outlook.com>
2025-02-06 17:52:01 +0800

    🚀优化移动端高度更改时的页面逻辑

--- scripts/device.js
+++ scripts/device.js
@@ -1,9 +1,13 @@
+let mobileMode = false;
+
 // 检查设备类型并设置显示状态
 function checkDeviceType() {
-    if (window.innerWidth <= 768) {
+    if (window.innerWidth <= 768 && !mobileMode) {
+        mobileMode = true;
         document.querySelector(".explorer").style.display = "none";
         document.querySelector(".editor-area").style.display = "none";
-    } else {
+    } else if (window.innerWidth > 768 && mobileMode) {
+        mobileMode = false;
         document.querySelector(".explorer").style.display = "block";
         document.querySelector(".editor-area").style.display = "block";
     }
