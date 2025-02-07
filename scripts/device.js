57bad123c5597ee7c690a15e56eee40c5bbffd84
pylxu <e-pylxu@outlook.com>
2025-02-07 09:55:59 +0800

    ⭕同步云服务中的实用性更改

--- scripts/device.js
+++ scripts/device.js
@@ -2,9 +2,17 @@ let mobileMode = false;
 
 // 检查设备类型
 function checkDeviceType() {
-    if (window.innerWidth <= 768 && !mobileMode) {
-        return true;
-    } else {
-        return false;
-    }
+  if (window.innerWidth <= 768 && !mobileMode) {
+    return true;
+  } else {
+    return false;
+  }
 }
+function resize() {
+var bodyHeight = window.innerHeight;
+document.body.style.height = bodyHeight + "px";
+}
+
+window.onresize = resize;
+
+resize();
